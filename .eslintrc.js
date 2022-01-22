96671b4310643cb6b2ee70fbef1331cf93f5e60b
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-01-22 16:22:00 +0900

    ESLint / Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
