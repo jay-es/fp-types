1febf682ff6ed53cdb8788547f49a06d81367c69
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-01-25 00:38:50 +0900

    新 Option 型

---
+++ src/oop/Option.spec.ts
@@ -0,0 +1,155 @@
+import { describe, expect, it, vi } from "vitest";
+import { Option } from "./Option";
+
+describe("Option", () => {
+  describe("isSome, isNone", () => {
+    it("Some", () => {
+      const some = Option.some("foo");
+
+      expect(some.isSome()).toBe(true);
+      expect(some.isNone()).toBe(false);
+    });
+
+    it("None", () => {
+      const none = Option.none();
+
+      expect(none.isSome()).toBe(false);
+      expect(none.isNone()).toBe(true);
+    });
+  });
+
+  describe("value", () => {
+    it("Some", () => {
+      const some = Option.some("foo");
+
+      expect(some.value("bar")).toBe("foo");
+    });
+
+    it("None", () => {
+      const none = Option.none();
+
+      expect(none.value("bar")).toBe("bar");
+    });
+  });
+
+  describe("get", () => {
+    it("Some", () => {
+      const some = Option.some("foo");
+
+      expect(some.get()).toBe("foo");
+    });
+
+    it("None", () => {
+      const none = Option.none();
+
+      expect(() => none.get()).toThrow();
+    });
+  });
+
+  describe("equal", () => {
+    it("Some", () => {
+      const some1 = Option.some("foo");
+      const some2 = Option.some("foo");
+      const some3 = Option.some("bar");
+      const none = Option.none();
+
+      const isSameLength = (a: string, b: string) => a.length === b.length;
+
+      expect(some1.equal(some2)).toBe(true);
+      expect(some1.equal(some3)).toBe(false);
+      expect(some1.equal(some3, isSameLength)).toBe(true);
+      expect(some1.equal(none)).toBe(false);
+    });
+
+    it("None", () => {
+      const none1 = Option.none();
+      const none2 = Option.none();
+      const some = Option.some("foo");
+
+      expect(none1.equal(none2)).toBe(true);
+      expect(none1.equal(some)).toBe(false);
+    });
+  });
+
+  describe("bind", () => {
+    it("Some", () => {
+      const some = Option.some("foo");
+      const bound = some.bind((value) => Option.some(value.endsWith("a")));
+
+      expect(bound.equal(Option.some(false))).toBe(true);
+    });
+
+    it("None", () => {
+      const none = Option.none();
+      const bound = none.bind((value) => Option.some(7));
+
+      expect(bound.equal(Option.none())).toBe(true);
+    });
+  });
+
+  describe("map", () => {
+    it("Some", () => {
+      const some = Option.some("foo");
+      const mapped = some.map((value) => value.endsWith("a"));
+
+      expect(mapped.equal(Option.some(false))).toBe(true);
+    });
+
+    it("None", () => {
+      const none = Option.none();
+      const mapped = none.map((value) => 7);
+
+      expect(mapped.equal(Option.none())).toBe(true);
+    });
+  });
+
+  describe("fold", () => {
+    it("Some", () => {
+      const some = Option.some("foo");
+      const folded = some.fold(true, (value) => value.endsWith("a"));
+
+      expect(folded).toBe(false);
+    });
+
+    it("None", () => {
+      const none = Option.none();
+      const folded = none.fold(42, (value) => 7);
+
+      expect(folded).toBe(42);
+    });
+  });
+
+  describe("iter", () => {
+    it("Some", () => {
+      const some = Option.some("foo");
+      const mock = vi.fn();
+      some.iter((value) => mock(value));
+
+      expect(mock).toBeCalledWith("foo");
+    });
+
+    it("None", () => {
+      const none = Option.none();
+      const mock = vi.fn();
+      none.iter((value) => mock());
+
+      expect(mock).not.toBeCalled();
+    });
+  });
+
+  // describe("toResult", () => {
+  //   it("Some", () => {
+  //     const some = Option.some("foo");
+  //     const result = some.toResult(42);
+
+  //     expect(result.equal(new Ok("foo"))).toBe(true);
+  //   });
+
+  //   it("None", () => {
+  //     const none = Option.none();
+  //     const result = none.toResult(42);
+
+  //     expect(result.equal(new Err(42))).toBe(true);
+  //   });
+  // });
+});
---
+++ src/oop/Option.ts
@@ -0,0 +1,90 @@
+import { equalFn } from "~/src/shared/utils";
+
+const vvv = Symbol();
+
+type Some<T> = {
+  [vvv]: T;
+};
+
+type None = {
+  [vvv]?: undefined;
+};
+
+type Type = "Some" | "None";
+
+export class Option<T> {
+  readonly #type: Type;
+  readonly [vvv]?: T;
+
+  private constructor(type: Type, value?: T) {
+    this.#type = type;
+    this[vvv] = value;
+  }
+
+  static some<T>(value: T): Option<T> {
+    return new this("Some", value);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static none<T = any>(): Option<T> {
+    return new this("None");
+  }
+
+  isSome(): this is Some<T> {
+    return this.#type === "Some";
+  }
+
+  isNone(): this is None {
+    return this.#type === "None";
+  }
+
+  value(defaultValue: T): T {
+    return this.isSome() ? this[vvv] : defaultValue;
+  }
+
+  get(): T {
+    if (this.isSome()) {
+      return this[vvv];
+    }
+
+    throw new Error("Cannot get value of None");
+  }
+
+  equal(other: Option<T>, fn: (v1: T, v2: T) => boolean = equalFn): boolean {
+    if (this.isSome() && other.isSome()) {
+      return fn(this[vvv], other[vvv]);
+    }
+
+    return this.isNone() && other.isNone();
+  }
+
+  bind<U>(fn: (value: T) => Option<U>): Option<U> {
+    if (this.isSome()) {
+      return fn(this[vvv]);
+    }
+
+    return Option.none<U>();
+  }
+
+  map<U>(fn: (value: T) => U): Option<U> {
+    if (this.isSome()) {
+      return Option.some(fn(this[vvv]));
+    }
+
+    return Option.none<U>();
+  }
+
+  fold<U>(none: U, fn: (value: T) => U): U {
+    if (this.isSome()) {
+      return fn(this[vvv]);
+    }
+
+    return none;
+  }
+
+  iter(fn: (value: T) => void): void {
+    if (this.isSome()) {
+      fn(this[vvv]);
+    }
+  }
+}
